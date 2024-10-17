4fc87e7932b37b4359098bda28b723f1bbdb7ade
Dohyung0 <162238754+Dohyung0@users.noreply.github.com>
2024-10-17 15:29:14 +0900

    Update hw3.c

--- hw3.c
+++ hw3.c
@@ -1,13 +1,19 @@
 #include<stdio.h>
 int main(void) {
 	int i, j, k;
+	const int NUM_LINE = 25;
 
-	for (i = 1;i <= 5;i++) {
-		for (j = i;j < 5;j++) {
+	for (i = 1;i <= NUM_LINE;i++) {
+		for (j = i;j < NUM_LINE;j++) {
 			printf(" ");
 		}
 		for (k = 1;k <= 2 * i - 1; k++) {
-			printf("*");
+			if (k == 1 || k == 2 * i - 1) {
+				printf("*");
+			}
+			else {
+				printf("-");
+			}
 		}
 		printf("\n");
 	}
